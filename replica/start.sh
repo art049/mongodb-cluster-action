8465bd6bbb87b8586fc6613f446e975634a27119
Arthur Pastel <arthur.pastel@gmail.com>
2023-12-18 15:14:26 +0100

    feat: allow mongo 5.0+ shards an replica

--- replica/start.sh
+++ replica/start.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 set -e
 printf "Waiting for an instance ."
-while ! docker-compose exec -T mongo0 mongo --eval "db.getMongo()" > /dev/null 2>&1
-do
+
+if docker-compose exec -T mongo0 mongo --version >/dev/null 2>&1; then
+  MONGO_CLI=mongo
+else
+  MONGO_CLI=mongosh
+fi
+
+while ! docker-compose exec -T mongo0 $MONGO_CLI --eval "db.getMongo()" >/dev/null 2>&1; do
   printf "."
-  sleep 1;
+  sleep 1
 done
 printf "OK\n"
 
 printf "Initializing the replicaset ..."
-docker-compose exec -T mongo0 mongo --quiet /scripts/init.js
+docker-compose exec -T mongo0 $MONGO_CLI --quiet /scripts/init.js
 printf "OK\n"
